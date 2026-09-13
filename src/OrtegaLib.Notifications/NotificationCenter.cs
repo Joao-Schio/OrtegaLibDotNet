5801ee479c6ef2434dd99386c4fcc10b5ae399b1
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-13 11:52:14 -0400

    add notification center

---
+++ src/OrtegaLib.Notifications/NotificationCenter.cs
@@ -0,0 +1,54 @@
+using OrtegaLib.Models;
+using OrtegaLib.Notifications.Common;
+
+namespace OrtegaLib.Notifications;
+
+public sealed class NotificationCenter
+{
+    private readonly string _serviceName;
+    private readonly INotificationSender _sender;
+
+    public NotificationCenter(
+        string serviceName,
+        INotificationSender sender)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(serviceName);
+        ArgumentNullException.ThrowIfNull(sender);
+
+        _serviceName = serviceName;
+        _sender = sender;
+    }
+
+    public ExceptionNotification CreateException(
+        Exception exception,
+        string? message = null)
+    {
+        ArgumentNullException.ThrowIfNull(exception);
+
+        return new ExceptionNotification(
+            _serviceName,
+            message ?? exception.Message,
+            exception);
+    }
+
+    public StatusNotification CreateStatus(
+        string message,
+        StatusNotification.Status status)
+    {
+        return new StatusNotification(
+            _serviceName,
+            message,
+            status);
+    }
+
+    public Task<Result<NotificationReceipt, NotificationError>> SendAsync(
+        Notification notification,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(notification);
+
+        return _sender.SendAsync(
+            notification,
+            cancellationToken);
+    }
+}
